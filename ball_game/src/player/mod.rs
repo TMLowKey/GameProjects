e3e3df0ea513b96522c40446fb10caa294082bf2
Lowkey-Laptop <marek.jtengler@gmail.com>
2024-05-24 15:40:32 +0200

    refactor: add system_sets
    Not necessary but System_sets is quiet useful for system ordering.

--- ball_game/src/player/mod.rs
+++ ball_game/src/player/mod.rs
@@ -5,16 +5,25 @@ pub mod systems;
 
 use systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player);
+        // Configure System sets
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet));
         app.add_systems(
             Update,
             (
-                player_movement,
-                confine_player_movement,
+                // Same can be done with .after, .before
+                player_movement.in_set(MovementSystemSet),
+                confine_player_movement.in_set(ConfinementSystemSet),
                 player_hit_star,
                 enemy_hit_player,
             ),
