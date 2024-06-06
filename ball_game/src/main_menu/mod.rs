46bcfc4a44ed403332593c0981d881a504710f5e
TMLowkey <marek.jtengler@gmail.com>
2024-06-06 14:34:44 +0200

    feat: add UI
    Add main menu, exit/start buttons

--- ball_game/src/main_menu/mod.rs
+++ ball_game/src/main_menu/mod.rs
@@ -1,13 +1,28 @@
+mod components;
+mod styles;
+mod systems;
+
+use systems::interactions::*;
+use systems::layout::*;
+
 use bevy::prelude::*;
+
+use crate::AppState;
 
 pub struct MainMenuPlugin;
 
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, main_menu);
+        app
+            // OnEnter State Systems
+            .add_systems(OnEnter(AppState::MainMenu), spawn_main_menu)
+            // Systems
+            .add_systems(
+                Update,
+                (interact_with_play_button, interact_with_quit_button)
+                    .run_if(in_state(AppState::MainMenu)),
+            )
+            // OnExit State Systems
+            .add_systems(OnExit(AppState::MainMenu), despawn_main_menu);
     }
-}
-
-pub fn main_menu() {
-    println!("You are on the main menu.");
 }
